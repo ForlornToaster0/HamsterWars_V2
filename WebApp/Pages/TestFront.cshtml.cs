3c4b7d8efe5152b982800b1a6d1a570c9383d205
TacoKrydda <92160516+TacoKrydda@users.noreply.github.com>
2022-05-23 14:38:39 +0200

    Testsida
    Bilder

---
+++ WebApp/Pages/TestFront.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class TestFrontModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
